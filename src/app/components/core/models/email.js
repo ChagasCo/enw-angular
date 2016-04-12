01af4e957cfdc66ff221410b15313600d753b584
chrisvfabio <chrisvfabio@gmail.com>
2016-04-12 21:32:33 +0800

    feat(email-service): add email service component to handle sending emails to the api endpoint
    closes #30

---
+++ src/app/components/core/models/email.js
@@ -0,0 +1,10 @@
+class Email {
+  constructor(name, email, phone, notes) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.notes = notes;
+  }
+}
+
+export default Email;
