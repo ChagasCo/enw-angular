1dba4f71efea64461c79c9caa177a9075db93bf7
chrisvfabio <chrisvfabio@gmail.com>
2016-04-16 13:08:17 +0800

    refactor(login ui): update login ui and error message
    Closes #45

--- src/app/components/login/login.controller.js
+++ src/app/components/login/login.controller.js
@@ -1,9 +1,10 @@
 class LoginController {
-  constructor($auth, $state, $stateParams) {
+  constructor($auth, $state, $stateParams, $scope) {
     'ngInject';
 
-    this.$auth = $auth;
-    this.$state = $state;
+    this.auth = $auth;
+    this.state = $state;
+    this.scope = $scope;
 
     this.credentials = {
       email: "",
@@ -16,16 +17,24 @@ class LoginController {
 
   login() {
     // Use Satellizer's $auth service to login
-    this.$auth.login(this.credentials)
+    this.auth.login(this.credentials)
       .then((data) => {
         // If login successful, redirect to the addProduct state
         if (angular.isDefined(this.redirectState)){
-          if(this.$state.get(this.redirectState)) {
-            this.$state.go(this.redirectState, {});
+          if(this.state.get(this.redirectState)) {
+            this.state.go(this.redirectState, {});
             return;
           }
         }
-        this.$state.go("home", {});
+        this.state.go("home", {});
+      })
+      .catch((error) => {
+        this.error = true;
+
+        // reset form
+        this.credentials = {};
+        this.scope.loginForm.$setPristine();
+        this.scope.loginForm.$setUntouched();
       });
   }
 }
